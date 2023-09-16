d490c577f6cb79ed4ec7e5a6ba91219cc6fb4f91
parthiv8848 <124236867+parthiv8848@users.noreply.github.com>
2023-09-16 14:45:25 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 // const mongoDbClient = require("mongodb").MongoClient
-const mongoURI = 'mongodb+srv://parthivkevadiya:UIZtYW0m8uMRDh5x@cluster0.ftkyalk.mongodb.net/Gofood' // Customer change url to your db you created in atlas
+const mongoURI = 'mongodb+srv://parthivkevadiya:aPhXHj718ERcyHjr@cluster0.ftkyalk.mongodb.net/Gofood' // Customer change url to your db you created in atlas
 // mongodb://<username>:<password>@merncluster-shard-00-00.d1d4z.mongodb.net:27017,merncluster-shard-00-01.d1d4z.mongodb.net:27017,merncluster-shard-00-02.d1d4z.mongodb.net:27017/?ssl=true&replicaSet=atlas-eusy5p-shard-0&authSource=admin&retryWrites=true&w=majority
 module.exports = function (callback) {
     mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
